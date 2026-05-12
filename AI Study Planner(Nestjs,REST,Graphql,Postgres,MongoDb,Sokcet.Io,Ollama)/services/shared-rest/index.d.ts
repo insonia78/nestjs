5e568b1ff1c2bc3413a8955a8a990b1c24277d03
thomas zangari <tomsa78@hotmail.it>
2026-05-11 20:17:58 -0400

    update

--- AI Study Planner(Nestjs,REST,Graphql,Postgres,MongoDb,Sokcet.Io,Ollama)/services/shared-rest/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ArgumentsHost, ExceptionFilter } from '@nestjs/common';
-
-export declare class AllExceptionsFilter implements ExceptionFilter {
-  private readonly logger;
-  catch(exception: unknown, host: ArgumentsHost): void;
-}
